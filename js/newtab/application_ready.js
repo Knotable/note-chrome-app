ccf208f50c94f02792cfe91867120b8d351a4730
long <forallandiyou@gmail.com>
2015-09-01 14:11:24 +0800

    fix: opens in too small window -- should be large enough that the display is not squished

--- js/newtab/application_ready.js
+++ js/newtab/application_ready.js
@@ -1,11 +1,10 @@
 'use strict';
 
-
 chrome.app.runtime.onLaunched.addListener(function() {
   chrome.app.window.create('newtab.html', {
-    'outerBounds': {
-      'width': 400,
-      'height': 500
+    'bounds': {
+      'width': Math.round(window.screen.availWidth*0.8),
+      'height': Math.round(window.screen.availHeight*0.8)
     }
   });
 });
